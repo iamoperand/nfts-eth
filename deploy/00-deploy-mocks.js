cf7e917b50f650e4fc81253aa29fc930a2ff56fe
nikhil arora <narora200@gmail.com>
2022-07-29 18:38:59 +0530

    feat: DynamicSvgNft deploy script

--- deploy/00-deploy-mocks.js
+++ deploy/00-deploy-mocks.js
@@ -4,13 +4,14 @@ const { developmentChains } = require("../helper-hardhat-config")
 const BASE_FEE = ethers.utils.parseEther("0.25") // 0.25 is the premium. It costs 0.25 LINK per request.
 const GAS_PRICE_LINK = 1e9 // link per gas
 
+const DECIMALS = "18"
+const INITIAL_PRICE = ethers.utils.parseUnits("2000", "ether")
+
 module.exports = async (hre) => {
     const { deployments, getNamedAccounts, network, ethers } = hre
 
     const { deploy, log } = deployments
     const { deployer } = await getNamedAccounts()
-
-    const args = [BASE_FEE, GAS_PRICE_LINK]
 
     // If we are on a development network, we need to deploy mocks!
     if (developmentChains.includes(network.name)) {
@@ -18,7 +19,12 @@ module.exports = async (hre) => {
         await deploy("VRFCoordinatorV2Mock", {
             from: deployer,
             log: true,
-            args,
+            args: [BASE_FEE, GAS_PRICE_LINK],
+        })
+        await deploy("MockV3Aggregator", {
+            from: deployer,
+            log: true,
+            args: [DECIMALS, INITIAL_PRICE],
         })
 
         log("Mocks Deployed!")
--- hardhat.config.js
+++ hardhat.config.js
@@ -12,7 +12,7 @@ const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 
 module.exports = {
-    solidity: "0.8.7",
+    solidity: { compilers: [{ version: "0.8.8" }, { version: "0.6.6" }] },
     defaultNetwork: "hardhat",
     networks: {
         hardhat: {
